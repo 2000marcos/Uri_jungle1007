f2ee4f8a13040d4845e076bdb6de10729a5d2724
perkles <souza.marcosv2@gmail.com>
2021-07-23 06:19:30 -0300

    primeiro commit

---
+++ URI_JUNGLE 1007/URI_JUNGLE 1007/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace URI_JUNGLE_1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, C, D, diferenca;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+
+            diferenca = (A * B - C * D);
+
+            Console.WriteLine("DIFERENCA = " + diferenca);
+            Console.ReadLine();
+
+        }
+    }
+}
